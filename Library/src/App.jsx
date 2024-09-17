ba074c644cebd9b3b0264dd81ea1a087e7701e7d
John <jbidlack@ccbcmd.edu>
2024-09-17 16:16:27 -0400

    starting Fresh

--- Library/src/App.jsx
+++ Library/src/App.jsx
@@ -1,41 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-
+  const [count, setCount] = useState(0)
 
   return (
-        <div className="mainDiv">
-          <div className="login">
-            <h1 className="title">Login</h1>
-            <h3 className="subTitle">Sign-In or join as a guest</h3>
-            <div>
-              <label htmlFor="un">Username: </label>
-              <input type="text" className="un" placeholder="Username" />
-            </div>
-            <div>
-              <label htmlFor="pw">Password: </label>
-              <input type="password" className="pw" placeholder="Password" />
-            </div>
-            <button type="submit">Login</button>
-          </div>
-        </div>
+    <>
+      <form typeof='login'>
+        
+      </form>
+    </>
   )
 }
 
 export default App
-
-
-{/* <div id="mainDiv">
-<div className="login">
-    <h1 className="title">Log in</h1>
-    <h3 className="subTitle">Sign-In or join as a guest</h3>
-  <div className="un">
-    <label htmlFor="username" className="un">Username: </label>
-    <input type="text" />
-  </div>
-  <div className="pw">
-    <label htmlFor="password" className="pw">Password: </label>
-    <input type="password" />
-  </div>
-</div>
-</div> */}
